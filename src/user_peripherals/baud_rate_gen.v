28dd033a7e01b529ab2f1ebea76b9accf1b3eccc
Goutham-Badhrinath <gouthambadhrinath123@gmail.com>
2025-08-30 22:51:49 +0530

    Update baud_rate_gen.v

--- src/user_peripherals/baud_rate_gen.v
+++ src/user_peripherals/baud_rate_gen.v
@@ -13,21 +13,21 @@ parameter TX_ACC_WIDTH = $clog2(TX_ACC_MAX);
 reg [RX_ACC_WIDTH - 1:0] rx_acc = 0;
 reg [TX_ACC_WIDTH - 1:0] tx_acc = 0;
 
-assign rxclk_en = (rx_acc == 5'd0);
-assign txclk_en = (tx_acc == 9'd0);
+	assign rxclk_en = (rx_acc == 6'd0);
+	assign txclk_en = (tx_acc == 10'd0);
 
 always @(posedge clk_50m) begin
 	if (rx_acc == RX_ACC_MAX[RX_ACC_WIDTH - 1:0])
 		rx_acc <= 0;
 	else
-		rx_acc <= rx_acc + 5'b1;
+		rx_acc <= rx_acc + 6'b1;
 end
 
 always @(posedge clk_50m) begin
 	if (tx_acc == TX_ACC_MAX[TX_ACC_WIDTH - 1:0])
 		tx_acc <= 0;
 	else
-		tx_acc <= tx_acc + 9'b1;
+		tx_acc <= tx_acc + 10'b1;
 end
 
 endmodule
